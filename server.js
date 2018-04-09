bb4a249b20977b74ed01848f45246598ed9f0e1b
Justin <justinho97@gmail.com>
2018-04-09 02:04:01 -0700

    initializing, setting up server, and working on handlebars

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var exphbs = require("express-handlebars");
+
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
+require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
+
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+});
